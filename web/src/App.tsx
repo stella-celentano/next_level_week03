41d01000a74df7d423682f35c056c72829cf2382
stella-celentano <stellacelentano99@gmail.com>
2020-10-14 18:38:30 -0300

    Finalizando aplicação WEB

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 // JSX - Java Script XML (coloca HTML dentro do JS)
 
 import './styles/global.css';
+import 'leaflet/dist/leaflet.css';
 
 import Routes from './routes'
 
